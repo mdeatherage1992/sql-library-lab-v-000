df46cab59042888aa03803c6fc6cab2720060bfb
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-03 12:43:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title,year FROM books WHERE series_id = 1 ORDER BY year" 
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -22,3 +22,31 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
 end
+
+
+
+
+
+# def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
+# "SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
+# end
+#
+# def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
+# "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name;"
+# end
+#
+# def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
+# "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_left FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING amount_left >= 0"
+# end
+#
+# def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
+#   "SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY SUM(pledges.amount);"
+# end
+#
+# def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
+# "SELECT projects.category, pledges.amount FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'music'"
+# end
+#
+# def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
+# "SELECT projects.category, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'books'"
+# end
