680210abc5cbc038faa4948fca00e39b6c4638c7
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-03 12:24:05 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,33 +1,33 @@
-CREATE TABLE series(
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  author_id INTEGER,
-  sub_genre INTEGER
-);
-
-CREATE TABLE authors(
-  id INTEGER PRIMARY KEY,
-  name TEXT
-);
-
-CREATE TABLE books(
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  series_id INTEGER,
-  year INTEGER
-);
-
-CREATE TABLE characters(
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  species TEXT,
-  motto TEXT,
-  author_id INTEGER,
-  series_id INTEGER
-);
-
-CREATE TABLE character_books(
-  id INTEGER PRIMARY KEY,
-  book_id TEXT,
-  character_id INTEGER
-);
+-- CREATE TABLE series(
+--   id INTEGER PRIMARY KEY,
+--   title TEXT,
+--   author_id INTEGER,
+--   sub_genre INTEGER
+-- );
+--
+-- CREATE TABLE authors(
+--   id INTEGER PRIMARY KEY,
+--   name TEXT
+-- );
+--
+-- CREATE TABLE books(
+--   id INTEGER PRIMARY KEY,
+--   title TEXT,
+--   series_id INTEGER,
+--   year INTEGER
+-- );
+--
+-- CREATE TABLE characters(
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   species TEXT,
+--   motto TEXT,
+--   author_id INTEGER,
+--   series_id INTEGER
+-- );
+--
+-- CREATE TABLE character_books(
+--   id INTEGER PRIMARY KEY,
+--   book_id TEXT,
+--   character_id INTEGER
+-- );
