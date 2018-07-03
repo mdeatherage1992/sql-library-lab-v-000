8cea543b7c8a081a8f7b02cf9a68969f14ac3aa7
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-03 12:53:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT COUNT(species) FROM characters ORDER by COUNT desc limit 1;"
+  "SELECT SPECIES,COUNT(*) FROM characters GROUP BY species ORDER BY COUNT(species) desc limit 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
