b5b8f440e01fb4f0f4b88c8d2a90959a81ec5eed
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-03 12:19:54 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,3 +4,15 @@ CREATE TABLE series(
   author_id INTEGER,
   sub-genre INTEGER
 );
+
+CREATE TABLE authors(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE s(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  sub-genre INTEGER
+);
